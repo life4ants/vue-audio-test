89b8d2f979756c54cac34746fbd29daea4c48f36
Andy Buck <brotherandy@live.com>
2017-05-03 12:20:01 -0400

    init

---
+++ src/components/Hello.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button @click="play">Click to play a sound</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      sound: null
+    }
+  },
+  mounted(){
+    this.sound = new Audio(require("../assets/sound.mp3"))
+  },
+  methods: {
+    play(){
+      this.sound.play()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
